86352c44397a0360474b28ac351934b5f53483f5
IlyaMokin <ilia.mokin@gmail.com>
2014-01-07 20:53:16 +0300

    изменил ссылки на относительные

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ var ONLINENETWORK = angular.module('ONLINENETWORK', ["ngRoute"],function () {
 }).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
 		$locationProvider.html5Mode(false);
-		$routeProvider.when('/home', { templateUrl: '/views/networkconstructor.htm' });
+		$routeProvider.when('/home', { templateUrl: 'views/networkconstructor.htm' });
 
 		$routeProvider.otherwise({ redirectTo: '/home'  });
 	} ]);
